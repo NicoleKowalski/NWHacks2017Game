dcf51afccdd70d298e17f08c0856dc6015e03825
NicoleKowalski <nicole.kowalski@live.com>
2017-03-18 16:14:26 -0700

    first commit
    first commit

---
+++ HACKATHON PROJECT/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position;
+        transform.forward = -player.transform.right;
+    }
+}
---
+++ HACKATHON PROJECT/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+}
